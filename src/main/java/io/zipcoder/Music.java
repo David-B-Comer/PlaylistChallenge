0194de882c14ca2c0667eb6e024f6f63de3d527a
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-02-20 16:47:55 -0500

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,27 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+    Integer goForward = 0;
+    Integer goBack = 0;
+    Integer Index;
+
+    for (int i = 0; i < playList.length; i++) {
+        if (selection.equals(playList[i])) {
+            Index = i;
+
+            if (Index > startIndex) {
+                goForward = Index - startIndex;
+
+                if (Index < playList.length -1) {
+                    goBack = playList.length -1 - Index;
+                }
+            }
+        }
     }
+    if (goForward > goBack && goBack !=0) {
+        return goBack;
+    }
+    else return goForward;
+}
 }
